bcf6f313521f5c9bc63c7a9bc24826df80518482
Schahzi35 <schehzi.ehmad@gmail.com>
2020-06-15 23:55:16 +0500

    Now ready to commit

---
+++ app.js
@@ -0,0 +1,171 @@
+//Chapter # 1
+//Task # 1.1
+//alert("Hello World!")
+
+//Task #1.2
+//alert ("Error! Please enter a valid password")
+
+//Task #1.3
+//alert ("Welcome to JS Land...\n Happy Coding!")
+
+//Task # 1.4
+//alert ("Welcome to JS Land...")
+//alert ("Happy Coding!")
+
+
+//Task # 1.5
+
+//console.log("Hello... I can run JS through my web browser's console");
+//alert("Hello... I can run JS through my web browser's console");
+
+//Chapter # 2
+//Task # 1
+
+//2.1
+
+//var a = username;
+
+//2.2
+//var myName = "Shahzad Ahmed Khan"
+
+//2.3
+
+//var message = "Hello World";
+//alert(message)
+
+//2.4
+
+//var firstname = "Johne";
+//var lastname = "Doe";
+//var fullname = (firstname + lastname);
+//alert (fullname);
+
+//var age = "15 years old";
+//alert(age)
+
+//var Course = "Certified Mobile Application Development";
+//alert(Course)
+
+//2.5
+
+//var arr = ["Pizza", "Pizz", "Piz", "Pi", "P"];
+//alert(arr)
+
+//2.6
+//var a = "My email address is ";
+//var email= "schehzi.ehmad@gmail.com";
+//alert(a+email)
+
+//2.7
+//var Book = "A smarter way to learn JavaScript";
+// var b = "I m trying to learn from the Book ";
+// alert(b+Book)
+
+//2.8
+// var a = "Yah! I can write HTML content through JavaScript";
+// document.write(a)
+
+// var a = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(a)
+
+//Chapter # 3
+
+//Task # 3.1
+
+// var age = "I m 34 years old";
+// alert(age)
+
+//Task # 3.2
+// escip
+
+//Task # 3.3
+
+// var birthYear = "1985";
+// var a = "My birth year is";
+// document.write(a+birthYear)
+
+//Task # 3.4
+// var name = "John Doe ";
+// var product = "5 T-shirts(s) ";
+// var quantity = "ordered ";
+// var storename = "on XYZ Cloting Store";
+// document.write(name+quantity+product+storename)
+
+//Chapter # 4
+
+//Task # 4.1
+// esc
+
+//Chapter # 5
+
+//Task # 5.1
+// var x = 3;
+// var y = 5;
+// var z = x+y;
+// document.write(z)
+
+//Task # 5.2
+// var x = 3;
+// var y = 5;
+// var z = x-y;
+// document.write(z)
+
+// var x = 3;
+// var y = 5;
+// var z = x*y;
+// document.write(z)
+
+// var x = 3;
+// var y = 5;
+// var z = x/y;
+// document.write(z)
+
+// var x = 3;
+// var y = 5;
+// var z = x % y;
+// document.write(z)
+
+//Task # 5.3
+
+// var a = 5;
+// document.write(a)
+
+// var a = 5;
+// c = 1+a;
+// document.write(c)
+
+// var c = 6;
+// var d = c++ + c;
+// document.write(d)
+
+// var d = 13;
+// var e = --d;
+// document.write(e)
+
+// var e = 12;
+// f = 12 % 3;
+// document.write(f)
+
+//Task # 5.4
+
+// var ticketCost = 600;
+// var quantity = 5
+// var totalcost = (ticketCost*quantity)
+// document.write(totalcost)
+
+//Task # 5.5
+
+// for (var i = 1; i <= 10; i++) {
+// document.write ("4" + "x" + i + "=" + 4*i + "<br>")
+// }
+
+//Task # 5.6
+// esc
+
+//Task # 5.7
+// Attn: Respected Sir! Muhammad Ali Mughal & Team,
+// Due to Corona Pendemic, Pichle hafte se me apni practice or assignment pori tarah nahi karsaka q k hamare ghar pe 02 fotgi hui, is waja se me assignment pora nahi karpaya, agr ap thora time extend kar dete tu bohat acha hota, 
+// kuch releif diya jaye.. bohat nawazish hogi.. Sir! me pichle 3 mahine se berozgar hu baraye meherbahi taowon karain..Thanx
+
+
+
